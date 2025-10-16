86541a84bd3170f6a695f2060f86d43655bf2186
karthikramx <karthikram570@gmail.com>
2025-10-16 18:17:15 +0530

    works now!

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import { useAuth } from "@/context/AuthContext";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Dashboard from "../dashboard/page";
@@ -18,15 +18,13 @@ export default function Home() {
 
   if (!user) return null;
 
-  console.log("this is from your home page");
-  console.log("this is the user id : ", user.uid);
-  console.log("this is the user object:", user);
-
   return (
-    <Dashboard>
-      <div className="w-full h-full flex flex-col">
-        <Dropzone></Dropzone>
-      </div>
-    </Dashboard>
+    <AuthProvider>
+      <Dashboard>
+        <div className="w-full h-full flex flex-col">
+          <Dropzone></Dropzone>
+        </div>
+      </Dashboard>
+    </AuthProvider>
   );
 }
--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,7 +1,5 @@
 import { collection, addDoc, DocumentData } from "firebase/firestore";
 import { db } from "./firebase";
-import { useAuth } from "@/context/AuthContext";
-import { fchown } from "fs";
 
 // firestore helper functions
 
@@ -11,14 +9,9 @@ export const createDocument = async (
   document: DocumentData
 ) => {
   try {
-    console.log("Code is from firestore.ts");
-    console.log(collectionName);
-    console.log(document);
     const docRef = await addDoc(collection(db, collectionName), document);
-    console.log(docRef);
     return docRef.id;
   } catch (error) {
-    console.log("Error creating documents!!!!@");
     console.log("Error creating document", error);
   }
 };
